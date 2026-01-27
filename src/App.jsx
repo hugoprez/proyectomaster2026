f14307194a6c57175c19959b7cf6871db4a48f91
Hugo Alberto Antillon Perez (SubDC) <hugoantillonprez@gmail.com>
2026-01-27 02:05:06 -0600

    feat: add navbar functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
 function App() {
   
   return (
     <>
+    <Router>
+      <Navbar/>
+      <Routes>
+      
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+    </Router>
+ 
     </>
   )
 }
